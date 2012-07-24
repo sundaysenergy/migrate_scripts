0a8908bb11a486ff64ac99aaa3cd21943431856b
Kai Curry <kai@sundaysenergy.com>
2012-07-24 12:55:23 -0500

    Migrate script wording update.

--- update_part2.sh
+++ update_part2.sh
@@ -15,70 +15,73 @@ echo
 drush updb -y
 echo
 
-echo "Saving a db snapshot called site-db-part2a.sql."
+#echo "Saving a db snapshot called site-db-part2a.sql."
 echo
 #drush sql-dump --skip-tables-key=common > site-db-part2a.sql
 echo
 
-echo "Enable the NISE Net Helper and related modules."
+echo "Enabling the NISE Net Helper and related modules."
 echo
-drush en -y nisenet_search nisenet_helper image_get_sizes nisenet_dashboard nisenet_img_styles nisenet_profile2 nisenet_core nisenet_dashboard nisenet_events
+drush en -y nisenet_search nisenet_helper image_legacy nisenet_dashboard nisenet_img_styles nisenet_profile2 nisenet_core nisenet_dashboard nisenet_events
 echo
 
-echo "Set the default theme back to NISE Net."
+echo "Setting the default theme back to NISE Net."
 echo
 drush vset theme_default "nisenet"
 echo
 
-echo "Enable admin and user inferface modules."
+echo "Enabling admin and user inferface modules."
 echo
 drush en -y admin_menu admin_menu_toolbar admin_views backup_migrate block_class contextual views_ui views_bulk_operations context_ui logintoboggan mollom module_filter
 echo
 
-echo "Enable other contributed modules"
+echo "Enabling other contributed modules in two batches."
 echo
 drush en -y beautytips beautytips_ui ckeditor compact_forms context_menu_block content_dashboard filefield_paths emfield media_vimeo file_force globalredirect lightbox2 print print_mail captcha googleanalytics flowplayer tagadelic
 drush en -y results_filter rules rules_admin site_map transliteration views_data_export xmlsitemap xmlsitemap_engines xmlsitemap_menu
 echo
 
-echo "Revert some views to what is in code. This updates them for d7."
+echo "Reverting all custom views to what is in nisenet_helper code."
+echo "This updates them for d7."
 echo
 drush vr catalog_featured catalog_most_viewed admin_content_comment admin_content_node admin_content front_catalog front_news mini_grants nodequeue_2 catalog_latest_grid admin_content_taxonomy admin_user_user
 echo
 
-echo "Revert some features."
+echo "Reverting all custom features."
 echo
 drush features-revert -y nisenet_search nisenet_img_styles nisenet_profile2 nisenet_core nisenet_dashboard nisenet_events
 echo
 
-echo "Take site back online"
-echo
-drush vset --always-set site_offline 0
-echo
-
-echo "Clear all drupal cache."
+echo "Clearing all drupal cache."
 echo
 drush cc all
 echo
 
-echo "Run cron."
+echo "Running cron."
 echo
 drush cron
 echo
 
-echo "Enable Migrate module for profile conversion."
+echo "Enabling Custom (nisenet_migrate) Migrate module for profile conversion."
 echo
 drush en -y nisenet_migrate
 echo
 
-echo "Clear all drupal cache."
+echo "Clearing all drupal cache."
 echo
 drush cc all
 echo
 
-echo "Migrate profiles to profile2 entities."
+echo "Migrating profiles to profile2 entities."
+echo
+echo "This can take several (5+) minutes."
 echo
 drush migrate-import NisenetContentProfile2Profile
+echo
+
+echo "Taking the site back online"
+echo
+drush vset --always-set site_offline 0
 echo
 
 LOGIN=`drush uli`
